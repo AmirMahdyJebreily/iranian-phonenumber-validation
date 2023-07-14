7141e4891b0d59b92524a3b3d9bd485b2ec6ddca
Amir JCS <amirmahdyjdx@gmail.com>
2023-07-14 17:17:12 +0330

    feat: add java script example

---
+++ Examples/JavaScript.js
@@ -0,0 +1,22 @@
+const regex = new RegExp("((0?9)|(\\+?989))((14)|(13)|(12)|(19)|(18)|(17)|(15)|(16)|(11)|(10)|(90)|(91)|(92)|(93)|(94)|(95)|(96)|(32)|(30)|(33)|(35)|(36)|(37)|(38)|(39)|(00)|(01)|(02)|(03)|(04)|(05)|(41)|(20)|(21)|(22)|(23)|(31)|(34)|(9910)|(9911)|(9913)|(9914)|(9999)|(999)|(990)|(9810)|(9811)|(9812)|(9813)|(9814)|(9815)|(9816)|(9817)|(998))\\W?\\d{3}\\W?\\d{4}", 'g')
+const str = `912 003 4965
+912-003-4965
++989120034965
++98912 003 4965
++98912-003-4965
+989120034965
+98912 003 4965
+98912-003-4965`;
+let m;
+
+while ((m = regex.exec(str)) !== null) {
+    // This is necessary to avoid infinite loops with zero-width matches
+    if (m.index === regex.lastIndex) {
+        regex.lastIndex++;
+    }
+    
+    // The result can be accessed through the `m`-variable.
+    m.forEach((match, groupIndex) => {
+        console.log(`Found match, group ${groupIndex}: ${match}`);
+    });
+}
